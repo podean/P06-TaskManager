96cb781748cf6ebcc8d6974c1340d25d5166ba50
15017167 <podean0957@gmail.com>
2017-05-30 16:43:58 +0800

    updated

--- app/src/main/java/com/example/potran/p06_taskmanager/AddActivity.java
+++ app/src/main/java/com/example/potran/p06_taskmanager/AddActivity.java
@@ -1,13 +1,81 @@
 package com.example.potran.p06_taskmanager;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
+
+    Button btnAdd, btnCancel;
+    EditText etName, etDes, etTime;
+
+    int rc = 12345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        etName = (EditText) findViewById(R.id.editTextName);
+        etDes = (EditText) findViewById(R.id.editTextDescription);
+        etTime = (EditText) findViewById(R.id.editTextTime);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                String des = etDes.getText().toString();
+//                final RadioButton rbStars = (RadioButton)findViewById(selectedButtonId);
+                DBHelper db = new DBHelper(AddActivity.this);
+                db.insertTask(name,des);
+
+
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, Integer.parseInt(etTime.getText().toString()));
+
+                Intent intent = new Intent(AddActivity.this,
+                        ScheduledNotificationReceiver.class);
+                intent.putExtra("title",name);
+                intent.putExtra("des", des);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this, rc,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+
+
+                setResult(RESULT_OK, intent);
+                db.close();
+                finish();
+
+
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AddActivity.this, MainActivity.class);
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/potran/p06_taskmanager/MainActivity.java
+++ app/src/main/java/com/example/potran/p06_taskmanager/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.potran.p06_taskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
+    ListView lvTask;
+    ArrayList<String> tasks;
+    ArrayAdapter<String> aaTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = (Button) findViewById(R.id.buttonAdd);
+
+        DBHelper db = new DBHelper(MainActivity.this);
+        tasks = db.getTaskContent();
+
+        lvTask = (ListView) findViewById(R.id.listViewTask);
+        aaTask = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tasks);
+        lvTask.setAdapter(aaTask);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivityForResult(i, 9);
+            }
+        });
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 9){
+            DBHelper db = new DBHelper(MainActivity.this);
+            tasks.clear();
+            tasks.addAll(db.getTaskContent());
+            db.close();
+            aaTask.notifyDataSetChanged();
+        }
+    }
+
+
+
 }
